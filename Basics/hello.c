11d2755ffcba0b37beea54ded04a13880624d940
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-03 17:35:43 +0530

    revising to take input

--- Basics/hello.c
+++ Basics/hello.c
@@ -6,6 +6,17 @@
 // }
 
 // Taking inputs
+// int main(void)
+// {
+//     // Create an integer variable that will store the number we get from the user
+//     int myNum;
+//     // Ask the user to type a number
+//     printf("Type a number: \n");
+//     // Get and save the number the user types
+//     scanf("%d", &myNum);
+//     // Output the number the user typed
+//     printf("Your number is: %d", myNum);
+// }
 
 // Comparing 2 numbers 
 // int main(void)
