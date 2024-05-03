12406f1bfab6478528e85f4c9500da3141acd40a
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-03 19:00:49 +0530

    added comments

--- Basics/mario_challenge.c
+++ Basics/mario_challenge.c
@@ -16,27 +16,35 @@ void space(int n){
 
 int main(void)
 {
-    // Taking user input 
+    // Taking user input until user inputs a number greater than or equal to 0
     int input;
     do {
         printf("Enter a number: \n");
         scanf("%d", &input);
     }while (input < 0);
 
+    // This main loop is to change from one row to another
     for (int i = 0; i < input; i++) {
+        // to print the initial spaces
         space(input-i+1);
 
+        // to print the "#" accordingly
         for (int j = 0; j < i+1; j++){
             printf("#");
         }
 
+        // Since we need to have two spaces between the walls
         space(2);
 
+        // again to print "#"
         for (int j = 0; j < i+1; j++){
             printf("#");
         }
 
+        // for spaces at the end 
         space(input-i+1);
+
+        // finally changing to next line
         printf("\n");
     }
 }
