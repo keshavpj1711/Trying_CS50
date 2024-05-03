82c819f61a4dd0d57db83b909d9791a8832ce6f0
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-03 17:31:38 +0530

    revising basics of c

--- Basics/hello.c
+++ Basics/hello.c
@@ -1,5 +1,42 @@
 #include<stdio.h>
 
-int main(){
-    printf("Hello World\n");
-}
+// Printing Hello World in C 
+// int main(void){
+//     printf("Hello World\n");
+// }
+
+// Taking inputs
+
+// Comparing 2 numbers 
+// int main(void)
+// {
+//     int num1 = 12;
+//     int num2 = 10;
+
+//     if (num1 > num2)
+//     {
+//         printf("num1 is greater than num2\n");
+//     }
+//     else if (num2 > num1)
+//     {
+//         printf("num2 is greater than num1\n");
+//     }
+//     else
+//     {
+//         printf("num1 and num2 are equal\n");
+//     }
+// }
+
+// Loops
+// int main(void){
+//     // int count = 3;
+//     // while (count > 0)
+//     // {
+//     //     printf("while loop working\n");
+//     //     count--;
+//     // }
+
+//     // for (int i = 0; i < 3; i++) {
+//     //     printf("for loop working\n");
+//     // }
+// }
