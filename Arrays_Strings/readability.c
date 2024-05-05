c7612d3e3b9b928ee9f9ea3605cd34a13d84686f
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-06 02:02:25 +0530

    added comments and a small fix

--- Arrays_Strings/readability.c
+++ Arrays_Strings/readability.c
@@ -38,41 +38,50 @@ int main(void)
     // In this program i am not taking note of actual length of the string 
     int num_letters=0, num_words=0, num_sentences=0;
 
-    // Punctuations that mark end of a sentence
-
+    // Punctuations that mark end of sentences are 
+    // ['!', '.', '?']
 
     // So we are going to look for first occurence of '\n'
     // By this we will find the len of out input string.
     while (str[num_chars] != '\n') {
         // Now inside this loop we can find other stuff to move on to next step
         // Like number of letters 
+        // We could have used ASCII values but why take the pain of memorizing that
         if ((str[num_chars] >= 'A' && str[num_chars] <= 'Z') || (str[num_chars] >= 'a' && str[num_chars] <= 'z')) {
             num_letters++;
         }
+        // Checking for number of spaces to guess the number of words
         else if (str[num_chars] == ' ') {
             num_words++;
         }
+        // Checking for the mentioned punctuations to get the number of sentences
         else if (str[num_chars] == '!'||str[num_chars] == '?'||str[num_chars] == '.') {
             num_sentences++;
         }
+
+        // incrementing as well as counting the number of chars
+        // Basically we are using it as a flag as well as counter
         num_chars++;
     }
 
     // At the end since num_words is equal to num_spaces+1 so,
     num_words++;
 
-    printf("Number of chars: %d\n", num_chars);
+    // printf("Number of chars: %d\n", num_chars);
     // printf("Number of letter: %d\n", num_letters);
     // printf("Number of words: %d\n", num_words);
     // printf("Number of sentences: %d\n", num_sentences);
 
     // Calculating L and S in Coleman-Liau index
+    // For logic of L and S refer to above explanation mentioned in the begining
     float L = (num_letters/(float)num_words)*100.0;
     float S = (num_sentences/(float)num_words)*100.0;
     float index = 0.0588 * L - 0.296 * S - 15.8;
 
+    // Rounding to nearest integer
     int grade_level = round(index);
 
+    // Applying the conditions required by the question
     if (index < 1) {
         printf("Before Grade 1\n");
     }
