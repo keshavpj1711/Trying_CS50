b81f78d0c846ce7de1c1be7c073b77ba78b4f77c
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-06 01:57:06 +0530

    completed readability even though still not optimized for variable lengths

--- Arrays_Strings/readability.c
+++ Arrays_Strings/readability.c
@@ -21,3 +21,66 @@
 // where L is the average number of letters per 100 words in the text, and 
 // S is the average number of sentences per 100 words in the text.
 
+#include <math.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // So our first problem to tackle is to take a big string as input.
+    int max_size = 400;
+    char str[max_size];
+    int num_chars = 0;
+
+    // Taking input 
+    printf("Enter paragraph: ");
+    fgets(str, max_size, stdin);
+
+    // In this program i am not taking note of actual length of the string 
+    int num_letters=0, num_words=0, num_sentences=0;
+
+    // Punctuations that mark end of a sentence
+
+
+    // So we are going to look for first occurence of '\n'
+    // By this we will find the len of out input string.
+    while (str[num_chars] != '\n') {
+        // Now inside this loop we can find other stuff to move on to next step
+        // Like number of letters 
+        if ((str[num_chars] >= 'A' && str[num_chars] <= 'Z') || (str[num_chars] >= 'a' && str[num_chars] <= 'z')) {
+            num_letters++;
+        }
+        else if (str[num_chars] == ' ') {
+            num_words++;
+        }
+        else if (str[num_chars] == '!'||str[num_chars] == '?'||str[num_chars] == '.') {
+            num_sentences++;
+        }
+        num_chars++;
+    }
+
+    // At the end since num_words is equal to num_spaces+1 so,
+    num_words++;
+
+    printf("Number of chars: %d\n", num_chars);
+    // printf("Number of letter: %d\n", num_letters);
+    // printf("Number of words: %d\n", num_words);
+    // printf("Number of sentences: %d\n", num_sentences);
+
+    // Calculating L and S in Coleman-Liau index
+    float L = (num_letters/(float)num_words)*100.0;
+    float S = (num_sentences/(float)num_words)*100.0;
+    float index = 0.0588 * L - 0.296 * S - 15.8;
+
+    int grade_level = round(index);
+
+    if (index < 1) {
+        printf("Before Grade 1\n");
+    }
+    else if (index > 16) {
+        printf("Grade 16+");
+    }
+    else {
+        printf("Grade %d\n", grade_level);
+    }
+}
+
