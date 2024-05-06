7bf6338c7a35d071fb87e0f3e292433805b40f54
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-06 19:29:32 +0530

    added all the conditionals to check for a valid key

--- Arrays_Strings/substitution.c
+++ Arrays_Strings/substitution.c
@@ -24,3 +24,67 @@
     // capitalized letters must remain capitalized letters; lowercase letters must remain lowercase letters.
 
     // After outputting ciphertext, you should print a newline. Your program should then exit by returning 0 from main.
+
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int is_key_allalpha(char str[], int len);
+int is_alpha_repeating(char str[], int len);
+
+int main(int argc, string argv[])
+{
+    int len_key = strlen(argv[1]);
+    
+    // Checking for a valid key 
+    // if argv[1] is empty
+    if (argv[1] == NULL) {
+        printf("Usage: ./substitution key\n");
+        return 1; // This is kind of our exit status code
+    } 
+    // if key is not 26 chars long
+    else if (len_key != 26) {
+        printf("Key should be 26 characters long\n");
+        return 1;
+    }
+    // checking for alphabets
+    else if (is_key_allalpha(argv[1], len_key) == 0) {
+        printf("All characters in the key should be alphabets\n");
+        return 1;
+    }
+    // checking if any alpha occurs more than once
+    else if (is_alpha_repeating(argv[1], len_key) == 0) {
+        printf("Each alphabet should occur only once\n");
+        return 1;
+    }
+    else {
+        // This is where we will actually proceed
+        
+    }
+
+}
+
+int is_key_allalpha(char str[], int len)
+{
+    // what i want to do is if any of the char is not alpha
+    // we return a zero value
+    for (int i = 0; i < len; i++) {
+        if (isalpha(str[i]) == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int is_alpha_repeating(char str[], int len)
+{
+    for (int i = 0; i < len; i++) {
+        for (int j = i; j < len; j++) {
+            if (toupper(str[i]) == toupper(str[j])) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
