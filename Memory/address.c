89c4cdd5e81994a81f6be4246e82b2438dacfcf7
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-11 02:04:07 +0530

    added declaring and using a pointer

--- Memory/address.c
+++ Memory/address.c
@@ -5,8 +5,18 @@ int main(void)
     int n = 5; 
 
     // Now what we want to do is look where this variable is actually stored
-    printf("Address of n: %p", &n);
+    // printf("Address of n: %p", &n);
     
     // Basically this "%p" is the format specifier for looking at the address of something.
     // or where is this thing pointing basically a pointer
+
+    // Syntax for declaring a pointer 
+    int *p = &n; // This * is different from the star used below
+
+    printf("p stores address of n: %p\n", p);
+    // Whereas
+    printf("Doing *p points to the value p holds: %d", *p);
+    // The * used is here actually points towards something 
+    // Like here it points to value of p (i.e it points to value of &n)
+    // And &n hold the value i.e. 5
 }
