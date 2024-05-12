23cfab4bd6946768369056b54890e33970a28812
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-12 21:05:10 +0530

    completed with turning photos to grayscale #BLM XD

--- Memory/filter-less/helpers.c
+++ Memory/filter-less/helpers.c
@@ -1,10 +1,28 @@
 // This is where the actual logic of functions is to be written
 
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Loop over all pixels
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Take average of red, green, and blue
+            float avg_color = (image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen)/3.0;
+            // Rounding off avg_color 
+            avg_color = round(avg_color);
+
+            // Update pixel values
+            image[i][j].rgbtRed = avg_color;
+            image[i][j].rgbtBlue = avg_color;
+            image[i][j].rgbtGreen = avg_color;
+        }
+    }
+
     return;
 }
 
