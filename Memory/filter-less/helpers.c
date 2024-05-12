78e130a74d6e996642fc3eb414bea545d0d16fab
Keshav Prajapati <keshavpj1711@gmail.com>
2024-05-12 21:19:32 +0530

    done with adding sepia effect but something seems broken

--- Memory/filter-less/helpers.c
+++ Memory/filter-less/helpers.c
@@ -29,6 +29,26 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    // To complete this task we must know the sepia formula that is 
+    // sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
+    // sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
+    // sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+
+    for (int i = 0; i < height; i++) {
+        
+        for (int j = 0; j < width; j++) {
+            
+            // Finding sepia rgb
+            float sepiaRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
+            float sepiaGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
+            float sepiaBlue = round(.272 * image[i][j].rgbtRed + .534 * image[i][j].rgbtGreen + .131 * image[i][j].rgbtBlue);
+
+            // Setting sepia colors to image
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtBlue = sepiaBlue;
+            image[i][j].rgbtGreen = sepiaGreen;
+        }
+    } 
     return;
 }
 
